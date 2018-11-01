42ad030fbcb9750203faefbe2103b073528e7c88
dtonal <dtonal@posteo.de>
2018-11-01 21:13:05 +0100

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+var express 	= require("express"),
+	app 		= express();
+
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || 'localhost';
+
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
+app.get("/", function(request, response){
+	response.send("index");
+});
+
+
+app.listen(port);
